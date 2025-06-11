f02f2f7f4bbcd7d34beb43a2d8268c51e138e7c3
Kazim Ali Tabrez Mir (Bit Quill Technologies Inc) <v-kmir@microsoft.com>
2025-06-11 09:45:30 -0700

    Refactored and added new tests and data for  Date, GroupBy operations, Value functions and other scenarios - Implemented GroupByWithListAverage query to calculate average LocationID by Borough. - Added output for GroupByWithListAverage to validate results. - Created GroupByWithListMinMax query to find max and min values for Zone and LocationID by Borough. - Added output for GroupByWithListMinMax to validate results. - Developed GroupByWithListMinMaxDateTime query to find max and min pickup datetime by PULocationID. - Added output for GroupByWithListMinMaxDateTime to validate results. - Implemented GroupByWithTableRunCount query to count occurrences of Zones. - Added output for GroupByWithTableRunCount to validate results. - Created ValueFunctions query to test various mathematical operations on passenger_count and total_amount. - Added output for ValueFunctions to validate results.

--- testframework/data/PQSDKTestFrameworkDataSchema.sql
+++ testframework/data/PQSDKTestFrameworkDataSchema.sql
@@ -1,35 +1,35 @@
-/*
-NOTE: 
-1) While uploading to the data source, all decimal values should have a scale of 2. That is, the number of digits after the decimal point should be 2. 
-2) All timestamp values should be uploaded to the datasouce in MM/DD/YYYY HH24:MI:SS format.
-*/
+CREATE TABLE NycTaxiData(
+	RecordID int,
+	VendorID int,
+	lpep_pickup_datetime timestamp,
+	lpep_dropoff_datetime timestamp,
+	store_and_fwd_flag boolean,
+	RatecodeID int,
+	PULocationID int,
+	DOLocationID int,
+	passenger_count int,
+	trip_distance double,
+	fare_amount double,
+	extra double,
+	mta_tax double,
+	tip_amount double,
+	tolls_amount double,
+	improvement_surcharge double,
+	total_amount double,
+	payment_type int,
+	trip_type int,
+	congestion_surcharge double
+);
 
-CREATE TABLE NycTaxiGreen (
-    RecordID int,
-    VendorID int,
-    lpepPickupDatetime timestamp,
-    lpepDropoffDatetime timestamp,
-    storeAndFwdFlag boolean,
-    RateCodeID int,
-    PULocationID int,
-    DOLocationID int,
-    passenger_count int,
-    trip_distance double,
-    fare_amount double,
-    extra double,
-    mta_tax double,
-    tip_amount double,
-    tolls_amount double,
-    improvement_surcharge double,
-    total_amount double,
-    payment_type int,
-    trip_type int,
-    congestion_surcharge double
+CREATE TABLE NycTaxiDateData (
+    RecordID int NOT NULL,
+	lpep_pickup_time date NOT NULL,
+	lpep_dropoff_time date NULL
 );
 
 CREATE TABLE TaxiZoneLookup (
-    LocationId int,
-    Borough string,
-    Zone string,
-    service_zone string	
+    LocationID int,
+	Borough string,
+	Zone string,
+	service_zone string
 );
